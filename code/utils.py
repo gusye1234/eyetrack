7cf5c00067ce2a5310bc873d37b0ae748418b2d1
dearmrlv <40029891+dearmrlv@users.noreply.github.com>
2019-08-22 23:29:41 +0800

    add QIR method

--- code/utils.py
+++ code/utils.py
@@ -95,7 +95,7 @@ def executeGC(img_name, filename, gamma):
 
 
 # Gamma Intensity Correction (GIC)
-def GIC(im_converted_name, im_canonical_name, save_name):
+def executeGIC(im_converted_name, im_canonical_name, save_name):
     """
     :type img: single channel image
     :rtype: single channel image
@@ -132,4 +132,56 @@ def GIC(im_converted_name, im_canonical_name, save_name):
     # plt.plot(sum_array)
     # plt.show()
     im_converted = executeGC(im_converted_name, save_name, sum_array.index(min(sum_array))*0.1)
+    
+ # Quotient Illumination Relighting (QIR)
+import glob
+def executeQIR(imGroup_0_loc, imGroup_1_loc, group_loc, new_im_name):
+    """
+    :type imGroup_0_loc: str, the directory of the canonical group
+    :type imGroup_1_loc: str,the directory of another group
+    :type group_loc: str, the directory for saveD
+    :type new_im_name: str, the pics will be named as new_im_name + str(index)
+    :rtype: None
+    :reference: https://ieeexplore.ieee.org/document/1240838
+    """
+    # 这个方法是通过比较一系列物体在两组不同光照条件下获得的照片，其中一组为标准组A，
+    # 这个函数的作用是模拟标准组A的光照条件去relight另一组B，从而获得组B物体在标准组A光照条件下的照片
+    # 需要指定一个标准组
+    # 两组内所有图片的shape都应该相同,图片数目应该相同，系列中的每个物体在两组中应该一一对应，即序号相同
+    # 如果我们的数据里面有不同光照条件下的多组数据，这个方法可能有用
+
+    imGroup_0 = glob.glob(imGroup_0_loc)    # type: array
+    imGroup_1 = glob.glob(imGroup_1_loc)    # type: array
+
+    im_num = len(imGroup_0)
+    R_mat = cv.imread(imGroup_0[0], 0)
+    R_mat = np.zeros_like(R_mat, dtype=float)
+    tmp_mat = np.zeros_like(R_mat, dtype=int)
+
+    r, c = R_mat.shape
+    im_1_dict = {}
+
+    for t in range(im_num):
+        im_0 = cv.imread(imGroup_0[t], 0)
+        im_1_dict[t] = cv.imread(imGroup_1[t], 0)
+        for i in range(r):
+            for j in range(c):
+                if im_0[i, j] != 0:
+                    R_mat[i, j] = R_mat[i, j] + 1/im_num * im_1_dict[t][i, j] / im_0[i, j]
+
+    for t in range(im_num):
+        for i in range(r):
+            for j in range(c):
+                if R_mat[i, j] != 0:
+                    tmp_mat[i, j] = int(im_1_dict[t][i, j] / R_mat[i, j])
+                else:
+                    tmp_mat[i, j] = int(im_1_dict[t][i, j])
+
+        img = tmp_mat.astype(np.int16)
+        img = Image.fromarray(img)
+        # img.show()
+        img = img.convert('L')
+        img.save(group_loc + new_im_name + str(t))
+        
+        
 
